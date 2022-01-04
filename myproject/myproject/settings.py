2e92698719daced3fea059da1f652f7a6d180f01
jeevanv94 <jeevan.v94@gmail.com>
2022-01-04 17:37:31 +1100

    test1

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -128,3 +128,11 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_jslint',
+    'django_jenkins.tasks.run_csslint',
+    'django_jenkins.tasks.run_sloccount'
+)
